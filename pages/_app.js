3211e1ece9a687dee92ec5c84bae58645d753258
Sean Ho <seanho96@yahoo.com>
2021-06-03 12:04:24 +0800

    change SSR to SSG

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,20 @@
 import React from 'react'
-import NextApp from 'next/app'
+// import NextApp from 'next/app'
 import { ThemeProvider } from 'styled-components'
-import { Client } from '../prismic-configuration'
+// import { Client } from '../prismic-configuration'
 import FirebaseContext from '../context/firebase'
 import { firebase, FieldValue } from '../lib/firebase'
 import { GlobalStyle, theme } from '../styles'
 
-export default class MyApp extends NextApp {
-  static async getInitialProps(appCtx) {
-    const client = Client()
-    const menu = (await client.getSingle('menu')) || {}
-    return {
-      props: {
-        menu: menu
-      }
-    }
-  }
-
-  render() {
-    const { Component, pageProps, props } = this.props
-    return (
-      <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Component {...pageProps} menu={props.menu} />
-        </ThemeProvider>
-      </FirebaseContext.Provider>
-    )
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </FirebaseContext.Provider>
+  )
 }
+
+export default MyApp
