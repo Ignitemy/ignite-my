7893078dd70d3acde30daf50ea5fc871dfd6ff8b
Sean Ho <seanho96@yahoo.com>
2021-06-03 10:33:28 +0800

    add rally schedule section

---
+++ pages/rally/_schedule.js
@@ -0,0 +1,119 @@
+import React from 'react'
+import styled from 'styled-components'
+import Image from 'next/image'
+import { Heading } from '../../components'
+
+const SectionContainer = styled.section`
+  width: 100%;
+  padding: 8rem 0;
+  background-color: ${(props) => props.bgcolor || 'var(--color-white)'};
+  background: ${(props) => props.background};
+`
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const StyledHeading = styled(Heading)`
+  -webkit-text-stroke: 3px ${(props) => props.stroke}; /* stroke width and color */
+  -webkit-font-smoothing: antialiased;
+  color: transparent;
+
+  @media (max-width: 900px) {
+    font-size: 4.8rem;
+    line-height: 3.6rem;
+  }
+`
+
+const ScheduleWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  max-width: 112rem;
+  margin-top: 4.8rem;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+`
+
+const SummitWrapper = styled.div`
+  margin-right: 3.8rem;
+`
+
+const RallyWrapper = styled.div`
+  display: flex;
+
+  @media (max-width: 900px) {
+    justify-content: flex-end;
+    margin-top: 2rem;
+  }
+`
+
+const DateWrapper = styled.div`
+  display: flex;
+  margin-top: 4.8rem;
+`
+
+const LeftDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 2rem;
+`
+const RightDate = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const IgnitesYou = () => {
+  return (
+    <>
+      <SectionContainer bgcolor="var(--color-black)">
+        <Container>
+          <StyledHeading
+            size="6.4rem"
+            align="center"
+            weight="bold"
+            lh="7.2rem"
+            stroke="var(--color-white)"
+          >
+            SCHEDULE
+          </StyledHeading>
+          <ScheduleWrapper>
+            <SummitWrapper>
+              <Image src="/images/png/summit-schedule-dark.png" width={533} height={528} />
+            </SummitWrapper>
+            <RallyWrapper>
+              <Image src="/images/png/rally-schedule-dark.png" width={538} height={528} />
+            </RallyWrapper>
+          </ScheduleWrapper>
+          <DateWrapper>
+            <LeftDate>
+              <Heading color="white" as="h4" size="4.8rem" lh="5rem" align="right">
+                4th
+              </Heading>
+              <Heading color="white" as="h4" size="4.8rem" lh="5rem" align="right">
+                Sept
+              </Heading>
+            </LeftDate>
+            <RightDate>
+              <Heading color="white" as="h4" size="4.8rem" lh="5rem" align="left">
+                20
+              </Heading>
+              <Heading color="white" as="h4" size="4.8rem" lh="5rem" align="left">
+                21
+              </Heading>
+            </RightDate>
+          </DateWrapper>
+        </Container>
+      </SectionContainer>
+    </>
+  )
+}
+
+export default IgnitesYou
--- pages/rally/index.js
+++ pages/rally/index.js
@@ -1,7 +1,12 @@
 import Layout from '../../components/Layout'
+import Schedule from './_schedule'
 
 const Rally = () => {
-  return <Layout title="Ignite 2021 | Rally"></Layout>
+  return (
+    <Layout title="Ignite 2021 | Rally">
+      <Schedule />
+    </Layout>
+  )
 }
 
 export default Rally
