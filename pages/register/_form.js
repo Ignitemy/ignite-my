87fec26298999b8d922e2f088291b9521d8445e5
Sean Ho <seanho96@yahoo.com>
2021-06-10 00:46:33 +0800

    add formik, yup and form validation

--- pages/register/_form.js
+++ pages/register/_form.js
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
-import { TextField, FormControlLabel, Checkbox } from '@material-ui/core'
+import { TextField, Checkbox } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
 import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles'
 import { useRouter } from 'next/router'
 import FirebaseContext from '../../context/firebase'
 import { doesEmailExist } from '../../helpers/firebase'
 import { Button, Text } from '../../components'
+import Modal from './_modal'
+import { Formik, Form, Field, useField, ErrorMessage } from 'formik'
+import * as yup from 'yup'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,7 +36,7 @@ const Container = styled.div`
   padding: 2.4rem;
 `
 
-const StyledForm = styled.form`
+const StyledForm = styled(Form)`
   width: 100%;
 `
 
@@ -41,69 +44,103 @@ const StyledAlert = styled(Alert)`
   margin-bottom: 2rem;
 `
 
+const CustomTextField = ({ ...props }) => {
+  const [field, meta] = useField(props)
+  const errorText = meta.error && meta.touched ? meta.error : ''
+  return (
+    <TextField
+      variant="outlined"
+      color="main"
+      style={{ width: '98%' }}
+      {...props}
+      {...field}
+      helperText={errorText}
+      error={!!errorText}
+    />
+  )
+}
+
+const CheckboxGroup = styled.div`
+  display: flex;
+  align-items: center;
+
+  label {
+    font-size: 1.2rem;
+
+    a {
+      text-decoration: none;
+      color: teal;
+    }
+  }
+`
+
+const StyledErrorMessage = styled(ErrorMessage)`
+  color: red;
+`
+
+const validationSchema = yup.object({
+  fullName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+  age: yup.string().max(2, "You can't be that old...").required(),
+  myKad: yup.number().required(),
+  contactNumber: yup.string().max(14).required(),
+  address: yup.string().required(),
+  city: yup.string().required(),
+  checked: yup.bool().oneOf([true], 'You have to check this to prcoeed')
+})
+
 const RegistrationForm = () => {
   const classes = useStyles()
   const router = useRouter()
   const { firebase } = useContext(FirebaseContext)
 
-  const [fullName, setFullName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [age, setAge] = useState('')
-  const [myKad, setMyKad] = useState('')
-  const [contactNumber, setContactNumber] = useState('')
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [checked, setChecked] = useState(false)
+  const [show, setShow] = useState(false)
   const [error, setError] = useState('')
   const [registered, setRegistered] = useState(false)
 
-  const handleSignUp = async (event) => {
-    event.preventDefault()
+  const handleSignUp = async (values, actions) => {
+    // event.preventDefault()
+    actions.setSubmitting(true)
 
-    const emailExists = await doesEmailExist(email)
+    const emailExists = await doesEmailExist(values.email)
+    console.log(values)
     if (!emailExists) {
       try {
         const createdUserResult = await firebase
           .auth()
-          .createUserWithEmailAndPassword(email, password)
+          .createUserWithEmailAndPassword(values.email, values.password)
 
         // authentication
         // -> emailAddress & password & fullName (displayName)
         await createdUserResult.user.updateProfile({
-          displayName: fullName
+          displayName: values.fullName
         })
 
         // firebase user collection (create a document)
         await firebase.firestore().collection('users').add({
           userId: createdUserResult.user.uid,
-          fullName,
-          email,
-          age,
-          myKad,
-          contactNumber,
-          address,
-          city,
+          fullName: values.fullName,
+          email: values.email,
+          age: values.age,
+          myKad: values.myKad,
+          contactNumber: values.contactNumber,
+          address: values.address,
+          city: values.city,
           dateCreated: Date.now()
         })
         setRegistered(true)
+        actions.setSubmitting(false)
 
         setTimeout(() => {
           router.push('/')
         }, 3000)
       } catch (error) {
-        setFullName('')
-        setEmail('')
-        setPassword('')
-        setAge('')
-        setMyKad('')
-        setContactNumber('')
-        setAddress('')
-        setCity('')
+        actions.resetForm()
         setError(error.message)
       }
     } else {
-      setEmail('')
+      values.email = ''
       setError('That email is already taken, please try another.')
     }
   }
@@ -123,113 +160,104 @@ const RegistrationForm = () => {
             </Text>
           </StyledAlert>
         )}
-        <StyledForm className={classes.root} onSubmit={handleSignUp} method="POST">
-          <TextField
-            name="fullName"
-            label="Full Name"
-            placeholder="Your full name here"
-            value={fullName}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setFullName(target.value)}
-            required
-          />
-          <TextField
-            type="email"
-            name="email"
-            label="Email Address"
-            placeholder="Your email address here"
-            value={email}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setEmail(target.value)}
-            required
-          />
-          <TextField
-            type="password"
-            name="password"
-            label="Password"
-            placeholder="Your password here"
-            value={password}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setPassword(target.value)}
-            required
-          />
-          <TextField
-            type="number"
-            name="age"
-            label="Age"
-            placeholder="Your age name here"
-            value={age}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setAge(target.value)}
-            required
-          />
-          <TextField
-            type="number"
-            name="myKad"
-            label="MyKad Number"
-            placeholder="Your MyKad number here"
-            value={myKad}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setMyKad(target.value)}
-          />
-          <TextField
-            type="tel"
-            name="contactNumber"
-            label="Contact Number"
-            placeholder="Your contact number here"
-            value={contactNumber}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setContactNumber(target.value)}
-          />
-          <TextField
-            name="address"
-            label="Address"
-            placeholder="Your address here"
-            value={address}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setAddress(target.value)}
-          />
-          <TextField
-            name="city"
-            label="City"
-            placeholder="Your city here"
-            value={city}
-            variant="outlined"
-            color="main"
-            style={{ width: '98%' }}
-            onChange={({ target }) => setCity(target.value)}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={checked}
-                onChange={() => setChecked(!checked)}
-                name="checked"
-                color="primary"
+        <Formik
+          initialValues={{
+            fullName: '',
+            email: '',
+            age: '',
+            myKad: '',
+            contactNumber: '',
+            address: '',
+            city: '',
+            checked: false
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => handleSignUp(values, actions)}
+        >
+          {({ resetForm, resetErrors, values, errors, isSubmitting, dirty, isValid }) => (
+            <StyledForm className={classes.root}>
+              <Field
+                name="fullName"
+                label="Full Name"
+                placeholder="Your full name here"
+                required
+                as={CustomTextField}
               />
-            }
-            label="I have informed my parent/guardian of the statements"
-            style={{ width: '100%' }}
-          />
-          <Button orange type="submit">
-            Register For Event
-          </Button>
-        </StyledForm>
+              <Field
+                type="email"
+                name="email"
+                label="Email Address"
+                placeholder="Your email address here"
+                required
+                as={CustomTextField}
+              />
+              <Field
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="Your password here"
+                required
+                as={CustomTextField}
+              />
+              <Field
+                type="number"
+                name="age"
+                label="Age"
+                placeholder="Your age name here"
+                required
+                as={CustomTextField}
+              />
+              <Field
+                type="number"
+                name="myKad"
+                label="MyKad Number"
+                placeholder="Your MyKad number here"
+                required
+                as={CustomTextField}
+              />
+              <Field
+                type="tel"
+                name="contactNumber"
+                label="Contact Number"
+                placeholder="Your contact number here"
+                required
+                as={CustomTextField}
+              />
+              <Field
+                name="address"
+                label="Address"
+                placeholder="Your address here"
+                required
+                as={CustomTextField}
+              />
+              <Field
+                name="city"
+                label="City"
+                placeholder="Your city here"
+                required
+                as={CustomTextField}
+              />
+              <CheckboxGroup>
+                <Field type="checkbox" name="checked" as={Checkbox} />
+                <label htmlFor="checked">
+                  Accept{' '}
+                  <a href="#" onClick={() => setShow(true)}>
+                    Terms & Conditions
+                  </a>
+                </label>
+              </CheckboxGroup>
+              <StyledErrorMessage name="checked" component="div" />
+
+              {/* <pre>{JSON.stringify(values, null, 2)}</pre>
+              <pre>{JSON.stringify(errors, null, 2)}</pre> */}
+              <Button orange type="submit" disabled={!isValid || !dirty || isSubmitting}>
+                Register For Event
+              </Button>
+            </StyledForm>
+          )}
+        </Formik>
       </Container>
+      <Modal show={show} closeModal={() => setShow(false)} />
     </ThemeProvider>
   )
 }
