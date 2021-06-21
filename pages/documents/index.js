0ac4bb91a974e7035452916f30c0301126fee25f
Sean Ho <seanho96@yahoo.com>
2021-06-21 14:01:07 +0800

    add documents page

---
+++ pages/documents/index.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout'
+
+const Documents = () => {
+  return (
+    <Layout title="Ignite 2021 | Documents">
+      <h1>Documents Page</h1>
+    </Layout>
+  )
+}
+
+export default Documents
