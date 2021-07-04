14792e315f3b4b715ab3cb474fa53254df7dad93
Sean Ho <seanho96@yahoo.com>
2021-07-04 15:28:39 +0800

    update copywriting

--- pages/home/_speakers.js
+++ pages/home/_speakers.js
@@ -85,15 +85,23 @@ const Speakers = () => {
         </StyledHeading>
       </Container>
       <CardContainer>
-        <Cards name="#SPEAKER 1">
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-          consequuntur magni dolores eos qui ratione voluptatem.
+        <Cards name="Eddryll Teo">
+          Eddyrll is currently the Head of MPU unit in MCKL. He has the burden to disciple the young
+          generation of Malaysians to know Jesus as their Lord and saviour. He founded Malaysian
+          Christian Memes and Malaysian Christian Talks as means to disciple alongside with his
+          team. He is currently focusing on practical and pastoral theology and has a desire to grow
+          Christians who are sound in faith and to train them to be disciple makers themselves as
+          commanded in Matthew 28.
         </Cards>
-        <Cards name="#SPEAKER 2">
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-          consequuntur magni dolores eos qui ratione voluptatem.
+        <Cards name="John Oomen" src="/images/jpg/john.jpg">
+          Best known as the former frontman of Malaysian pop band Paperplane Pursuit, John O has a
+          decade and a half of experience in the music industry as an award-winning songwriter,
+          producer and performer. He recently kicked off a new phase of his career as a solo artist,
+          writing and producing old school soul with a fresh pop sound. As a follower of Jesus, it
+          has been his lifelong passion to do his small part in bringing the reality of God to the
+          world outside the church, particularly the entertainment industry.
         </Cards>
-        <Cards name="#SPEAKER 3">
+        <Cards name="Agnel Raj">
           Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
           consequuntur magni dolores eos qui ratione voluptatem.
         </Cards>
@@ -105,10 +113,16 @@ const Speakers = () => {
         <Heading color="white" size="3.6rem" align="center" ls="8px" mb="3.6rem">
           SPECIAL ITEM
         </Heading>
-        <SpecialCard name="#SPECIAL SPEAKER">
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-          consequuntur magni dolores eos qui ratione voluptatem. Nemo enim ipsam voluptatem quia
-          voluptas sit aspernatur aut odit aut fugit.
+        <SpecialCard name="Juwita Suwito" src="/images/jpg/juwita.jpg">
+          Juwita is a sought-after singer-songwriter, vocal coach, performer and podcaster with a
+          heart for worship. She has released four studio and live albums, and has also been
+          featured in regional television, movie soundtracks as well as gospel albums. Although her
+          latest solo album, This Side of Heaven was released back in 2015, her singles such as
+          Stand, Belle of the Ball and Just Like That continue to be heard on radio.
+          <br />
+          Juwita seeks to be a light to the nation and works out her passion to bring Malaysian
+          music and inspiration back to the heart of its people through Four Forty Records and its
+          collaborative initiatives such as Songs & Stories and the Malaysian Worship Collective.
         </SpecialCard>
       </SpecialItemContainer>
     </SectionContainer>
