5b9bbd21b8c30454048d10ea9a889001810ef5c3
Sean Ho <seanho96@yahoo.com>
2021-07-04 15:29:48 +0800

    feat(cards): pop up cards

--- pages/home/_cards.js
+++ pages/home/_cards.js
@@ -8,11 +8,11 @@ const SpeakerCard = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  border-radius: 2.5rem;
   align-items: center;
+  border-radius: 2.5rem;
   min-width: 300px;
   margin-top: 2.4rem;
-
+  position: relative;
   transition: transform 0.3s;
 
   &:hover {
@@ -23,32 +23,51 @@ const SpeakerCard = styled.div`
 const SpeakerContent = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  position: absolute;
+  bottom: 0;
+  max-height: 76px;
+  overflow: hidden;
+  height: auto;
   background-color: var(--color-orange);
   width: 100%;
   max-width: 370px;
   padding: 2.4rem;
   border-radius: 2.5rem;
-  margin-top: -51px;
   z-index: 1;
+  transition: all 0.5s ease-in;
+
+  :hover {
+    max-height: 407px;
+
+    p {
+      display: unset;
+    }
+  }
 `
 const StyledImage = styled(Image)`
   border-radius: 2.5rem;
+  object-fit: cover;
+  filter: grayscale(100%);
+`
+
+const HoverText = styled(Text)`
+  display: none;
 `
 
 const Cards = (props) => {
   const { src, name, children } = props
   return (
     <SpeakerCard>
-      <StyledImage src={src ? src : '/images/png/event-2.png'} height={256} width={370} />
+      <StyledImage src={src ? src : '/images/png/event-2.png'} height={407} width={370} />
       <SpeakerContent>
         <Heading color="black" size="1.8rem">
           {name}
         </Heading>
-        <Text color="white" mt="2rem">
+        <HoverText color="white" mt="2rem">
           {children}
-        </Text>
+        </HoverText>
       </SpeakerContent>
     </SpeakerCard>
   )
