206722d378e480292cb651a9b994f81fbe42a394
Sean Ho <seanho96@yahoo.com>
2021-07-10 10:50:45 +0800

    add OG image

--- components/Layout.js
+++ components/Layout.js
@@ -18,6 +18,13 @@ const Layout = ({ children, title }) => {
           name="description"
           content="IGNITE Youth Leadership Summit is a collaboration between NextGen, the Youth Ministry of Damansara Utama Methodist Church, and Scripture Union Peninsular Malaysia. Much like its name, IGNITE seeks to inspire Christian students to be catalysts of change in their schools by sparking influence among Malaysian youths."
         ></meta>
+        <meta
+          property="og:image"
+          content="https://ignitemy.live/images/png/og-image.png"
+          width={453}
+          height={170}
+        />
+        <meta property="og:type" content="website" />
         <link
           rel="preload"
           href="/fonts/Gotham-Light.woff2"
