7bed61d305eb47725ffd29b77a841c147b26d9ea
Ashley Lai <ashleylpm@hotmail.com>
2021-10-02 14:47:03 +0800

    highlights page

---
+++ pages/highlights/index.js
@@ -0,0 +1,20 @@
+import Layout from '../../components/Layout'
+import HeaderButtons from './_header-buttons'
+import Stats from './_stats'
+import Logo from './_logo'
+import Quotes from './_quotes'
+import Video from './_video'
+
+const Highlights = () => {
+  return (
+    <Layout title="IGNITEMY2021 | Highlights">
+      <HeaderButtons />
+      <Stats/>
+      <Logo/>
+      <Quotes/>
+      <Video/>
+    </Layout>
+  )
+}
+
+export default Highlights
