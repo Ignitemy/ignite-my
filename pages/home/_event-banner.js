01b033dfc88c563d6d28a52143afe41e41e32dbb
Po YZ <poyizhi@gmail.com>
2023-04-12 16:59:08 +0800

    chore: remove motion from event banner component

--- pages/home/_event-banner.js
+++ pages/home/_event-banner.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
 
 const BannerContainer = styled.div`
   height: 60vh;
