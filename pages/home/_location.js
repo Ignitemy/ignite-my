8633798042c63f345f94d200a5892b2c68479dd9
Po YZ <poyizhi@gmail.com>
2023-04-12 17:15:48 +0800

    style: change text color, heading position and iframe size

--- pages/home/_location.js
+++ pages/home/_location.js
@@ -1,29 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
-import {HeadingShadow } from '../../components'
+import { HeadingShadow } from '../../components'
 
 const SectionWrapper = styled.div`
-  display: block;
+  /* display: block;
   align-items: center;
-  justify-content: center;
+  justify-content: center; */
   padding: 5rem 2rem;
-  background: url("/images/png/wave_location.png");
-  background-size: cover;
+  background-color: #ffffff;
+  /* background: url("/images/png/wave_location.png");
+  background-size: cover; */
   @media screen and (min-width: 768px) {
     padding: 5rem 5rem;
   }
 
   @media screen and (min-width: 1024px) {
-    display: flex;
+    /* display: flex; */
     padding: 5rem 8rem;
   }
 
   @media screen and (min-width: 1440px) {
-    padding: 5rem 14rem;
+    padding: 7rem 14rem;
   }
 
   @media screen and (min-width: 1750px) {
-    padding: 7rem 24rem;
+    padding: 8rem 24rem;
+  }
+`
+
+const ContentWrapper = styled.div`
+  display: block;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (min-width: 1024px) {
+    display: flex;
   }
 `
 
@@ -51,20 +62,19 @@ const LeftSection = styled.div`
 const RightSection = styled.div`
   padding-top: 2rem;
 
-  @media screen and (min-width: 1024px) {
-    padding-top: 7rem;
-  }
+  /* @media screen and (min-width: 1024px) {
+    padding-top: 2rem;
+  } */
 `
 
 const TextContainer = styled.p`
   font-size: 1.6rem;
-  color: var(--color-white);
+  color: var(--color-black);
   padding: 0.5rem 0;
   line-height: 3.6rem;
   @media screen and (min-width: 768px) {
     font-size: 2rem;
   }
-
 `
 const StyledIframe = styled.iframe`
   width: 100%;
@@ -76,8 +86,8 @@ const StyledIframe = styled.iframe`
   }
 
   @media screen and (min-width: 1440px) {
-    width: 476px;
-    height: 321px;
+    width: 600px;
+    height: 450px;
   }
 `
 
@@ -90,29 +100,34 @@ const Location = () => {
   ]
   return (
     <SectionWrapper>
-      <LeftSection>
-        <HeadingShadow>Getting there</HeadingShadow>
-        <div>
-          <SubheaderWrapper>Location</SubheaderWrapper>
-          {address.map((addressLine) => (
-            <TextContainer key={addressLine}>{addressLine}</TextContainer>
-          ))}
-        </div>
-        <div>
-          <SubheaderWrapper>Parking spots</SubheaderWrapper>
-          <TextContainer>
-            There are available parking lots in Dream Centre, alternatively you may park at PJ33 or Symphony Square
-          </TextContainer>
-        </div>
-      </LeftSection>
-      <RightSection>
-        <StyledIframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.934015301823!2d101.6345276152521!3d3.112160954245589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4be03b52eca1%3A0x7ec210346197ea0a!2sDUMC%20Dream%20Centre!5e0!3m2!1sen!2smy!4v1652794551685!5m2!1sen!2smy"
-          allowfullscreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></StyledIframe>
-      </RightSection>
+      <HeadingShadow color="black" align="center">
+        Getting there
+      </HeadingShadow>
+      <ContentWrapper>
+        <LeftSection>
+          <div>
+            <SubheaderWrapper>Location</SubheaderWrapper>
+            {address.map((addressLine) => (
+              <TextContainer key={addressLine}>{addressLine}</TextContainer>
+            ))}
+          </div>
+          <div>
+            <SubheaderWrapper>Parking spots</SubheaderWrapper>
+            <TextContainer>
+              There are available parking lots in Dream Centre, alternatively you may park at PJ33 or
+              Symphony Square
+            </TextContainer>
+          </div>
+        </LeftSection>
+        <RightSection>
+          <StyledIframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.934015301823!2d101.6345276152521!3d3.112160954245589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4be03b52eca1%3A0x7ec210346197ea0a!2sDUMC%20Dream%20Centre!5e0!3m2!1sen!2smy!4v1652794551685!5m2!1sen!2smy"
+            allowfullscreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></StyledIframe>
+        </RightSection>
+      </ContentWrapper>
     </SectionWrapper>
   )
 }
