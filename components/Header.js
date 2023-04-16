7581204979012affac5b8a4803601b22bfd38c93
Po YZ <poyizhi@gmail.com>
2023-04-16 22:27:18 +0800

    refactor: uncomment registration button and comment stream live button

--- components/Header.js
+++ components/Header.js
@@ -225,16 +225,16 @@ const Header = () => {
                   Log In
                 </Button>
               </StyledLink>
-              {/* <StyledLink href="/register">
+              <StyledLink href="/register">
                 <RegisterButton orange="true">Register</RegisterButton>
-              </StyledLink> */}
-              <StyledExtLink
+              </StyledLink>
+              {/* <StyledExtLink
                 href="https://ignitemy.online.church/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <RegisterButton orange="true">Streaming Live</RegisterButton>
-              </StyledExtLink>
+              </StyledExtLink> */}
             </>
           ) : (
             <>
