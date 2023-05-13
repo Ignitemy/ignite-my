99ca54e8ef595d9a71814396611c76f927042082
Po YZ <poyizhi@gmail.com>
2023-05-13 15:31:44 +0800

    chore: show event highlight section in highlight

--- pages/highlights/index.js
+++ pages/highlights/index.js
@@ -1,10 +1,11 @@
 import Layout from '../../components/Layout'
-import HeaderButtons from './_header-buttons'
-import Stats from './_stats'
-import Logo from './_logo'
-import Quotes from './_quotes'
+// import HeaderButtons from './_header-buttons'
+// import Stats from './_stats'
+// import Logo from './_logo'
+// import Quotes from './_quotes'
 import Video from './_video'
 import Gallery from './_gallery'
+import EventHighlights from './_event-highlights'
 
 const Highlights = () => {
   return (
@@ -14,6 +15,7 @@ const Highlights = () => {
       {/* <Logo /> */}
       {/* <Quotes /> */}
       <Video />
+      <EventHighlights />
       <Gallery />
     </Layout>
   )
